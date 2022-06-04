1594e39bebf61e113218a3e3c3365f1e040ce14e
Mateus-Souza-Silva <mateussousa54321@gmail.com>
2022-06-03 21:48:19 -0300

    Primeiro commit

--- App.js
+++ App.js
@@ -1,20 +1,49 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { View, StyleSheet, FlatList, ActivityIndicator } from 'react-native';
+import api from './src/services/api';
+import Carros from './src/Carros/carros';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      carros: [],
+      loading: true
+    };
+  }
+  async componentDidMount() {
+    const response = await api.get('');
+    this.setState({
+      carros: response.data,
+      loading: false
+    });
+  }
+
+  render() {
+    if (this.state.loading) {
+      return (
+        <View style={{ alignItems: 'center', justifyContent: 'center', flex: 1 }}>
+          <ActivityIndicator color="#09A6FF" size={40} />
+        </View>
+      )
+    } else {
+      return (
+        <View style={styles.container}>
+          <FlatList
+            data={this.state.carros}
+            keyExtractor={item => item.id.toString()}
+            renderItem={({ item }) => <Carros data={item} />}
+          />
+        </View>
+      )
+    }
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+  }
 });
+
+export default App; 
